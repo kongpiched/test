eec723f7df27dcc2c2b6dc047a7c8a8248942c8a
bubblefew <bubble.few@gmail.com>
2023-07-07 21:27:21 +0700

    few

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.listen(3000,() =>{
-    console.log ("Sever Start on port :3000");
-})
+
 
 app.get("/test",(req,res)=>{
     res.json("Hello World");
+})
+
+app.listen(3000,() =>{
+    console.log ("Sever Start on port :3000");
 })
